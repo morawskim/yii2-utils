b38af7a57057de50b381f42ea3239bd0b7d88d9f
Marcin Morawski <marcin@morawskim.pl>
2019-08-19 19:03:03 +0200

    Add dataProvider with allowed IPs

--- tests/filters/IpRequestTest.php
+++ tests/filters/IpRequestTest.php
@@ -10,14 +10,19 @@ use yii\web\Request;
 
 class IpRequestTest extends \mmo\yii2\tests\TestCase
 {
-    public function testIpIsAllowed(): void
+    /**
+     * @dataProvider dataProviderAllowed
+     * @param array $allowedIPs
+     * @param string $clientIp
+     * @throws ForbiddenHttpException
+     */
+    public function testIpIsAllowed(array $allowedIPs, string $clientIp): void
     {
-        $allowedIp = '192.168.0.2';
         $controller = new Controller('id', \Yii::$app);
         $action = new Action('test', $controller);
         $filter = new IpRequest([
-            'allowedIPs' => [$allowedIp],
-            'request' => $this->mockRequest($allowedIp)
+            'allowedIPs' => $allowedIPs,
+            'request' => $this->mockRequest($clientIp)
         ]);
         $this->assertTrue($filter->beforeAction($action));
     }
@@ -34,18 +39,6 @@ class IpRequestTest extends \mmo\yii2\tests\TestCase
         $filter->beforeAction($action);
     }
 
-    public function testIpRangeIsBlocked(): void
-    {
-        $controller = new Controller('id', \Yii::$app);
-        $action = new Action('test', $controller);
-        $filter = new IpRequest([
-            'allowedIPs' => ['192.168.1.*'],
-            'request' => $this->mockRequest('192.168.15.15')]
-        );
-        $this->expectException(ForbiddenHttpException::class);
-        $filter->beforeAction($action);
-    }
-
     protected function mockRequest(string $userIp): Request
     {
         /** @var Request $request */
@@ -54,5 +47,14 @@ class IpRequestTest extends \mmo\yii2\tests\TestCase
             ->getMock();
         $request->method('getUserIP')->willReturn($userIp);
         return $request;
+    }
+
+    public function dataProviderAllowed(): array
+    {
+        return [
+            'simple' => [['192.168.0.2'], '192.168.0.2'],
+            'wildcard' => [['192.168.15.*'], '192.168.15.15'],
+            'many_ips' => [['192.168.2.*', '192.168.3.3'], '192.168.3.3'],
+        ];
     }
 }
